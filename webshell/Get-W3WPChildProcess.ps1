c54537ae33b03879e87f5a87738d05c2443e3cd5
CatOnAcidd <69175691+CatOnAcidd@users.noreply.github.com>
2024-06-23 09:02:12 +1200

    Create Get-W3WPChildProcess.ps1

---
+++ webshell/Get-W3WPChildProcess.ps1
@@ -0,0 +1,10 @@
+While($true)
+{
+    If ($(Get-WmiObject -Class Win32_Process -Filter "Name='w3wp.exe'") -ne $null)
+    {
+        $p = Get-WmiObject -Class Win32_Process -Filter "Name='w3wp.exe'" | Select ProcessID,CreationDate
+        $pp = $p.ProcessId
+        Get-WmiObject -Class Win32_Process -Filter "ParentProcessId=$pp" |`
+        ? ($_.Name -eq "cmd.exe") | & C:\hunting\scripts\Get-W3WPChildren.ps1
+    }
+}
